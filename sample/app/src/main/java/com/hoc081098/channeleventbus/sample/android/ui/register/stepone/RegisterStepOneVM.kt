d50ce62ed5499514979a959b019d6b9cc5952a4b
hoc081098@gmail.com <hoc081098@gmail.com>
2023-11-24 01:09:41 +0700

    basic android sample

--- sample/app/src/main/java/com/hoc081098/channeleventbus/sample/android/ui/register/stepone/RegisterStepOneVM.kt
+++ sample/app/src/main/java/com/hoc081098/channeleventbus/sample/android/ui/register/stepone/RegisterStepOneVM.kt
@@ -2,9 +2,14 @@ package com.hoc081098.channeleventbus.sample.android.ui.register.stepone
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hoc081098.channeleventbus.ChannelEventBus
 import com.hoc081098.channeleventbus.sample.android.ui.register.SubmitFirstNameEvent
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
 
 class RegisterStepOneVM(
   private val savedStateHandle: SavedStateHandle,
@@ -13,12 +18,17 @@ class RegisterStepOneVM(
   internal val firstNameStateFlow: StateFlow<String?> = savedStateHandle.getStateFlow<String?>(FIRST_NAME_KEY, null)
 
   init {
-    addCloseable { channelEventBus.send(SubmitFirstNameEvent(null)) }
+    firstNameStateFlow
+      .onEach { channelEventBus.send(SubmitFirstNameEvent(it)) }
+      .onCompletion {
+        check(it is CancellationException)
+        channelEventBus.send(SubmitFirstNameEvent(null))
+      }
+      .launchIn(viewModelScope)
   }
 
   internal fun submitFirstName(value: String) {
     savedStateHandle[FIRST_NAME_KEY] = value
-    channelEventBus.send(SubmitFirstNameEvent(value))
   }
 
   companion object {
