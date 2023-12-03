13979da31bbf5220ea41f289edba4ac53d2b4c1c
hoc081098@gmail.com <hoc081098@gmail.com>
2023-12-04 00:30:25 +0700

    spotlessApply

--- channel-event-bus/build.gradle.kts
+++ channel-event-bus/build.gradle.kts
@@ -39,8 +39,8 @@ kotlin {
   }
 
   // According to https://kotlinlang.org/docs/native-target-support.html
-  // Tier 1﻿: macosX64, macosArm64, iosSimulatorArm64, iosX64
-  // Tier 2﻿: linuxX64, linuxArm64,
+  // Tier 1: macosX64, macosArm64, iosSimulatorArm64, iosX64
+  // Tier 2: linuxX64, linuxArm64,
   //         watchosSimulatorArm64, watchosX64, watchosArm32, watchosArm64,
   //         tvosSimulatorArm64, tvosX64, tvosArm64,
   //         iosArm64
