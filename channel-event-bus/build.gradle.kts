17eb1dadab1478f35ae334b07d0ca16c6e0cfce4
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2023-12-04 00:18:32 +0700

    More targets (#15)

--- channel-event-bus/build.gradle.kts
+++ channel-event-bus/build.gradle.kts
@@ -38,12 +38,25 @@ kotlin {
     nodejs()
   }
 
+  // According to https://kotlinlang.org/docs/native-target-support.html
+  // Tier 1﻿: macosX64, macosArm64, iosSimulatorArm64, iosX64
+  // Tier 2﻿: linuxX64, linuxArm64,
+  //         watchosSimulatorArm64, watchosX64, watchosArm32, watchosArm64,
+  //         tvosSimulatorArm64, tvosX64, tvosArm64,
+  //         iosArm64
+  // Tier 3: androidNativeArm32, androidNativeArm64, androidNativeX86, androidNativeX64,
+  //         mingwX64,
+  //         watchosDeviceArm64
+
   iosArm64()
   iosX64()
   iosSimulatorArm64()
 
   macosX64()
   macosArm64()
+  mingwX64()
+  linuxX64()
+  linuxArm64()
 
   tvosX64()
   tvosSimulatorArm64()
@@ -53,6 +66,12 @@ kotlin {
   watchosArm64()
   watchosX64()
   watchosSimulatorArm64()
+  watchosDeviceArm64()
+
+  androidNativeArm32()
+  androidNativeArm64()
+  androidNativeX86()
+  androidNativeX64()
 
   sourceSets {
     commonMain {
