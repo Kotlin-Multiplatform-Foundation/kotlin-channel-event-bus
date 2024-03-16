fae1dac45b3c0b2941a5ae158c291bb43ada8ebe
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2024-03-16 18:56:45 +0700

    Add wasmJs support (#41)
    * Add wasm support
    
    * Update build.gradle.kts
    
    * implementation(kotlin("test-wasm-js"))

--- channel-event-bus/build.gradle.kts
+++ channel-event-bus/build.gradle.kts
@@ -27,13 +27,23 @@ kotlin {
       }
     }
   }
+
   js(IR) {
-    compilations.all {
-      kotlinOptions {
-        sourceMap = true
-        moduleKind = "commonjs"
+    moduleName = property("POM_ARTIFACT_ID")!!.toString()
+    compilations.configureEach {
+      compilerOptions.configure {
+        sourceMap.set(true)
+        moduleKind.set(org.jetbrains.kotlin.gradle.dsl.JsModuleKind.MODULE_COMMONJS)
       }
     }
+    browser()
+    nodejs()
+  }
+  @OptIn(org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl::class)
+  wasmJs {
+    // Module name should be different from the one from JS
+    // otherwise IC tasks that start clashing different modules with the same module name
+    moduleName = property("POM_ARTIFACT_ID")!!.toString() + "Wasm"
     browser()
     nodejs()
   }
@@ -92,6 +102,12 @@ kotlin {
     jsTest {
       dependencies {
         implementation(kotlin("test-js"))
+      }
+    }
+
+    val wasmJsTest by getting {
+      dependencies {
+        implementation(kotlin("test-wasm-js"))
       }
     }
 
